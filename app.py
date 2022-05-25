6c5b70f84a657b1c87f1f7aa1195950ee1b103b4
Leo <leodutton@Leos-MacBook-Pro.local>
2022-05-25 12:12:06 +0100

    updates to app.py to abide by clean code. added .gitignore to ignore __pycache__

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
+from asyncore import read
 import os
+import re
 import boto3
 import pandas as pd
 
+_S3_PATH = 's3://'
+_BUCKET = 'awstrainingbkt'
 
-url = 'awstrainingbkt/enrolment1/2020/month=01/myFile_17.csv'
+def create_aws_file_path(aws_file_path: str):
+    """Joins the s3 path, bucket name, and path to file into a single string
 
-data = pd.read_csv('s3://'+url)
+    Args:
+        aws_file_path (str): path to file within s3 bucket
 
-print(data.head())
+    Returns:
+        str: full path of file stored on s3
+    """
+    full_path = os.path.join(_S3_PATH, _BUCKET, aws_file_path)
+    return full_path
+
+def read_csv_from_aws(aws_file_path: str):
+    """_summary_
+
+    Args:
+        aws_file_path (str): path to file within s3 bucket
+
+    Returns:
+        df: dataframe of csv file loaded from s3
+    """
+    full_path = create_aws_file_path(aws_file_path)
+    data = pd.read_csv(full_path)
+
+    return data
+
+
+read_csv_from_aws('/enrolment1/2020/month=01/myFile_17.csv')
